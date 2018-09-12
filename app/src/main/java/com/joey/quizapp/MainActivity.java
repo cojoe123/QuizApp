77393cc70c495409f75915d5b9d74f8ed7c00aa3
Joseph Zambrano <cojoe123@yahoo.com>
2018-09-12 19:42:54 -0400

    Added multiple questions and imgs

--- app/src/main/java/com/joey/quizapp/MainActivity.java
+++ app/src/main/java/com/joey/quizapp/MainActivity.java
@@ -4,12 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button trueButton;
     private Button falseButton;
+    private ImageButton nextButton;
+    private ImageButton prevButton;
+    private TextView questionTextView;
+
+    private Question[] questionBank = new Question[] {
+      new Question(R.string.question_australia, true),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_primes, true),
+            new Question(R.string.question_ligthning, false),
+            new Question(R.string.question_afterdeath, false),
+            new Question(R.string.question_dogs, true)
+    };
+
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,21 +34,76 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         trueButton = (Button) findViewById(R.id.true_button);
-        falseButton = (Button) findViewById(R.id.false_button);
-
         trueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
 
+        falseButton = (Button) findViewById(R.id.false_button);
         falseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
 
+        // have button display next question when clicked
+        nextButton = (ImageButton) findViewById(R.id.next_button);
+        questionTextView = (TextView) findViewById(R.id.question_text);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+
+        // when text is clicked next question appears
+        questionTextView = (TextView) findViewById(R.id.question_text);
+        questionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex + 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+
+        // prevButton to handle going back to previous question
+        prevButton = (ImageButton) findViewById(R.id.prev_button);
+        questionTextView = (TextView) findViewById(R.id.question_text);
+        prevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentIndex = (currentIndex - 1) % questionBank.length;
+                updateQuestion();
+            }
+        });
+
+        updateQuestion();
+
     }
+
+    private void updateQuestion() {
+        int question = questionBank[currentIndex].getTextResId();
+        questionTextView.setText(question);
+    }
+
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = questionBank[currentIndex].isAnswerTrue();
+
+        int messageResId = 0;
+
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
